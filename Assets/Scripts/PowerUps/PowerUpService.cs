1c4d1d4c89e1a932f563b7ad2fb91d46f823662f
salara-mirza <130211580+salara-mirza@users.noreply.github.com>
2025-09-15 23:48:44 +0530

    Introduce generic object pooling for PowerUps
    Refactored the object pooling system to use a generic approach instead of a hard-coded switch statement.

--- Assets/Scripts/PowerUps/PowerUpService.cs
+++ Assets/Scripts/PowerUps/PowerUpService.cs
@@ -8,12 +8,15 @@ namespace CosmicCuration.PowerUps
         private PowerUpScriptableObject powerUpScriptableObject;
         private bool isSpawning;
         private float spawnTimer;
+        PowerUpPool powerUpPool;
 
         public PowerUpService(PowerUpScriptableObject powerUpScriptableObject)
         {
             this.powerUpScriptableObject = powerUpScriptableObject;
+            powerUpPool = new PowerUpPool();
             spawnTimer = this.powerUpScriptableObject.spawnRate;
             isSpawning = true;
+           
         }
 
         public void Update()
@@ -50,11 +53,11 @@ namespace CosmicCuration.PowerUps
             switch (typeToFetch)
             {
                 case PowerUpType.Shield:
-                    return new Shield(fetchedData);
+                    return (PowerUpController) powerUpPool.GetPowerUp<Shield>(fetchedData ) as Shield;
                 case PowerUpType.DoubleTurret:
-                    return new DoubleTurret(fetchedData);
+                    return (PowerUpController) powerUpPool.GetPowerUp<DoubleTurret>(fetchedData) as DoubleTurret;
                 case PowerUpType.RapidFire:
-                    return new RapidFire(fetchedData);
+                    return (PowerUpController) powerUpPool.GetPowerUp<RapidFire>(fetchedData) as RapidFire  ;
                 default:
                     throw new Exception($"Failed to Create PowerUpController for: {typeToFetch}");
             }
@@ -77,5 +80,7 @@ namespace CosmicCuration.PowerUps
         }
 
         public void SetPowerUpSpawning(bool setSpawningActive) => isSpawning = setSpawningActive;
+
+        public void ReturnPowerUpToPool(PowerUpController powerUpToReturn) => powerUpPool.ReturnItem(powerUpToReturn);
     } 
 }
